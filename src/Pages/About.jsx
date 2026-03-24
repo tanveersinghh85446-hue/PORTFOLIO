73077b4c82706c4bc3b08298d54a8b659e0fd497
tanveersinghh85446-hue <tanveersinghh85446@gmail.com>
2026-03-24 17:26:54 +0530

    message

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const About = () => {
   return (
-    <section className="relative w-full min-h-screen flex items-center justify-center px-4 py-12 overflow-hidden">
-      
+    <section className="relative w-full min-h-screen  bg-[#08090d] flex items-center justify-center px-4 py-12 overflow-hidden">
+
       {/* Background Video */}
       <video
         autoPlay
@@ -19,26 +19,28 @@ const About = () => {
       <div className="absolute top-0 left-0 w-full h-full bg-none"></div>
 
       {/* Content */}
-      <div className="relative max-w-6xl w-full bg-none backdrop-blur-md rounded-2xl shadow-lg p-6 md:p-10">
-        
+      <div className="relative max-w-6xl w-full bg-none  rounded-2xl shadow-lg p-6 md:p-10">
+
         {/* Heading */}
         <div className="text-center mb-8">
           <h1 className="text-3xl md:text-4xl font-bold text-gray-800">
-            About Me
+            <span className="[-webkit-text-stroke:1px_rgba(79,110,247,0.7)] text-transparent">About Me</span>
           </h1>
-          <p className="text-gray-500 mt-2">
-            Web Developer | React | Tailwind
+          <p className="text-white mt-2">
+            | Full Stack Web Developer |
           </p>
         </div>
 
         {/* Content */}
         <div className="grid md:grid-cols-2 gap-8 items-center">
-          
+
           <div>
             <h2 className="text-2xl font-semibold text-gray-800 mb-3">
-              Who I Am
+
+              <span className="[-webkit-text-stroke:1px_rgba(79,110,247,0.7)] text-transparent">Who I Am</span>
+
             </h2>
-            <p className="text-gray-600 leading-relaxed">
+            <p className="text-white leading-relaxed">
               I'm a passionate web developer who loves building modern,
               responsive, and user-friendly websites. I enjoy turning ideas
               into real-world applications using React and Tailwind CSS.
@@ -49,7 +51,9 @@ const About = () => {
 
           <div>
             <h2 className="text-2xl font-semibold text-gray-800 mb-3">
-              My Skills
+              <span className="[-webkit-text-stroke:1px_rgba(79,110,247,0.7)] text-transparent">My Skills
+              </span>
+
             </h2>
 
             <div className="flex flex-wrap gap-2">
@@ -60,6 +64,15 @@ const About = () => {
               <span className="bg-gray-200 px-3 py-1 rounded-lg">Tailwind</span>
               <span className="bg-gray-200 px-3 py-1 rounded-lg">Node.js</span>
               <span className="bg-gray-200 px-3 py-1 rounded-lg">Git</span>
+              <span className="bg-gray-200 px-3 py-1 rounded-lg">PhotoShop</span>
+              <span className="bg-gray-200 px-3 py-1 rounded-lg">PowerPoint</span>
+              <span className="bg-gray-200 px-3 py-1 rounded-lg">Canva</span>
+              <span className="bg-gray-200 px-3 py-1 rounded-lg">sql</span>
+              <span className="bg-gray-200 px-3 py-1 rounded-lg">Excel</span>
+              <span className="bg-gray-200 px-3 py-1 rounded-lg">Coreldraw</span>
+              <span className="bg-gray-200 px-3 py-1 rounded-lg">Python</span>
+
+
             </div>
           </div>
         </div>
@@ -67,16 +80,26 @@ const About = () => {
         {/* Bottom Section */}
         <div className="mt-10 grid sm:grid-cols-2 gap-6">
           <div className="bg-gray-50 p-5 rounded-xl">
-            <h3 className="font-semibold text-lg mb-2">My Goal</h3>
-            <p className="text-gray-600">
+            <h3 className="font-semibold text-lg mb-2"></h3>
+            <span className="font-['Syne',sans-serif] text-xs font-bold tracking-[0.14em] uppercase text-indigo-400 flex items-center gap-2 mb-6
+                                        after:content-[''] after:h-px after:bg-white/10 after:inline-block after:max-w-15 after:flex-1">
+              My Goal
+            </span>
+            <p className="text-black font-bold">
               My goal is to become a full-stack developer and build scalable
               web applications that solve real-world problems.
             </p>
           </div>
 
           <div className="bg-gray-50 p-5 rounded-xl">
-            <h3 className="font-semibold text-lg mb-2">Currently Learning</h3>
-            <p className="text-gray-600">
+            <h3 className="font-semibold text-lg mb-2"></h3>
+            <span className="font-['Syne',sans-serif] text-xs font-bold tracking-[0.14em] uppercase text-indigo-400 flex items-center gap-2 mb-6
+                                        after:content-[''] after:h-px after:bg-white/10 after:inline-block after:max-w-15 after:flex-1">
+
+              Currently Learning
+            </span>
+
+            <p className="text-black font-bold">
               Currently learning backend development, APIs, databases,
               and improving React skills.
             </p>
